21436af1b66c5c034e32ad41d7892c5a5954af3e
r00t0k <artists2@naver.com>
2021-12-19 17:18:29 +0900

    [create] databases/models : Notices, Schedule

--- app/databases/models.py
+++ app/databases/models.py
@@ -1,1 +1,21 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Notices(Base):
+    __tablename__ = "notice_info"
+
+    id = Column(Integer, primary_key=True, unique=True)  # unique 설정 => id
+    date = Column(String(30))
+    title = Column(String(100))
+    link = Column(String(110))
+
+
+class Schedules(Base):
+    __tablename__ = "schedule_info"
+
+    id = Column(Integer, primary_key=True, unique=True)  # unique 설정 => id
+    date = Column(String(30))
+    title = Column(String(100))
